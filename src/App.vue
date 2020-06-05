688992f828be8bb1e87b5fe2daad57fb575ab2ab
fei <dpf1945744457@163.com>
2020-06-05 16:20:30 +0800

    set

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <!-- 显示主页 -->
+    <el-container class="app-container">
+      <!-- 左侧导航菜单 -->
+      <el-aside width="200px">Aside</el-aside>
+
+      <el-container>
+        <!-- 顶部区域 -->
+        <el-header class="app-header">Header</el-header>
+
+        <!-- 内容主体区域 -->
+        <el-main class="app-main">Main</el-main>
+
+        <!-- 底部区域-->
+        <el-footer class="app-footer">Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style >
+html,
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(247, 248, 250, 1);
+}
+#app {
+  height: 100%;
+
+}
+.app-container {
+  height: 100%;
+}
+.app-header {
+  padding-left: 0;
+  padding-right: 0;
+}
+.app-main {
+  padding-bottom: 0;
+  padding-right: 0;
+}
+.app-footer {
+  padding-bottom: 0;
+  padding-right: 0;
+}
+</style>
---
+++ src/components/layout/MyAside.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="aside">
+    <div class="title">
+      <p>HC小区管理系统</p>
+    </div>
+    <div v-for="(item,idx) in navs" :key="idx">
+      <router-link tag="span" :to="item.path" active-class="nav_on" exact>
+        <div class="nav">
+          <i class="fa" :class="item.icon"></i>
+          <span v-text="item.text"></span>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      navs: [
+        { path: "", text: "小区管理", icon: "fa-tag", id: 1 },
+        { path: "/shop", text: "资产管理", icon: "fa-calendar", id: 2 },
+        { path: "/good", text: "业主管理", icon: "fa-tag", id: 3 },
+        { path: "/order", text: "订单", icon: " fa-hand-o-left", id: 4 },
+        { path: "/customer", text: "客户", icon: "fa-home", id: 5 },
+        { path: "/ss", text: "数据", icon: "fa-book", id: 6 },
+        { path: "/ss", text: "资产", icon: " fa-hand-o-left", id: 7 },
+        { path: "/ss", text: "应用", icon: " fa-hand-o-left", id: 8 },
+        { path: "/ss", text: "设置", icon: " fa-hand-o-left", id: 9 }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
