66f575578052e449c4c9ac454390097adbfe3248
fei <dpf1945744457@163.com>
2020-06-05 20:07:09 +0800

    fix

---
+++ src/components/layout/MyFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- 伸缩按钮 -->
+    <div class="left_buttom">
+      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+        <el-radio-button :label="false">展开</el-radio-button>
+        <el-radio-button :label="true">收起</el-radio-button>
+      </el-radio-group>
+    </div>
+
+    <div>
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          丰仪家园
+          <i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>黄金糕</el-dropdown-item>
+          <el-dropdown-item>狮子头</el-dropdown-item>
+          <el-dropdown-item>螺蛳粉</el-dropdown-item>
+          <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+          <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
